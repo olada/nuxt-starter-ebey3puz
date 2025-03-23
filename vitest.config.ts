6677058caa5299470abd84e1094ac0866ffb1f50
olada <david.olah@gmail.com>
2025-03-23 23:54:39 +0100

    remove files which are not necessary for repro

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,5 @@ import { defineVitestConfig } from '@nuxt/test-utils/config';
 export default defineVitestConfig({
   test: {
     include: ['**/*.spec.ts'],
-    globals: true,
-    environment: 'jsdom'
   },
 });
